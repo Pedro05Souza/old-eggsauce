da99f5f79748472028fce7755bc051219046e518
Alexius Maliuk Dias <alexiusmd2004@gmail.com>
2024-04-02 13:51:12 -0300

    Add ban command

--- commands/modCommands.py
+++ commands/modCommands.py
@@ -45,7 +45,21 @@ class ModCommands(commands.Cog):
         if ctx.author.top_role.position <= ctx.guild.me.top_role.position or str(ctx.author.id) in self.devs:
             await User.kick()
             await ctx.send(f"{User.mention} foi kickado")
-
+    
+    @commands.command()
+    async def ban(self, ctx, User: discord.Member):
+        if ctx.author.top_role.position <= ctx.guild.me.top_role.position or str(ctx.author.id) in self.devs:
+            await User.ban()
+            await ctx.send(f"{User.mention} foi banido")
+        else:
+            try:
+                await ctx.me.guild.fetch_ban(User)
+            except(discord.ext.commands.errors.MemberNotFound):
+                await ctx.send(f"{User} não foi encontrado")
+            except(discord.HTTPException):
+                await ctx.send(f"Erro de resposta do servidor")
+            except(discord.Forbidden):
+                await ctx.send(f"{ctx.author} não tem permissões para fazer isso")
 
 async def setup(bot):
      await bot.add_cog(ModCommands(bot))
