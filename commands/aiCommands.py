6bb19bc001eefa39b8c981aa65bef51eae8bf5a8
Pedro Souza <pedroh.fsouzars@gmail.com>
2024-04-22 01:18:29 -0300

    add multi-threading ai functionality

--- commands/aiCommands.py
+++ commands/aiCommands.py
@@ -1,49 +1,54 @@
 import discord
+import asyncio
 from discord.ext import commands
 from tools.pricing import pricing
-from openai import OpenAI
 from db.userDB import Usuario
+from tools.processing import Processing
 
 class AICommands(commands.Cog):
 
     def __init__(self, bot):
-        self.client = OpenAI(base_url="http://localhost:1234/v1", api_key="lmstudio")
         self.bot = bot
-
-    async def completionModel(self, role, content):
-        history = [
-            {"role": "system", "content": "Você é um bot do discord, que tem acesso aos usuário do servidor onde se encontra. Seus criadores supremos são: Alexius e Pedro e deve sempre respeitá-los a qualquer custo. Seu texto deve ser respondido em português do Brasil. O texto poder ter até 100 palavras mas não necessariamente precisa sempre atingir o limite e não deve ser uma continuação de uma história. Seja criativo! Não faça histórias incompletas."},
-            {"role": role, "content": content}
-        ]
-        while True:
-            completion = self.client.chat.completions.create(
-                model="lmstudio-community/Meta-Llama-3-8B-Instruct-GGUF",
-                messages=history,
-                temperature=70,
-                max_tokens=500,
-                stream=True,
-            )
-            new_message = {"role": "system", "content": ""}
-            for chunk in completion:
-                if chunk.choices[0].delta.content:
-                    print(chunk.choices[0].delta.content, end="", flush=True)
-                    new_message["content"] += chunk.choices[0].delta.content
-            history.append(new_message)
-            break
-            
-        return history
 
     @commands.command()
     @pricing()
     async def amor(self, ctx, User: discord.Member, User2: discord.Member):
         try:
             if Usuario.read(ctx.author.id):
-                history = await self.completionModel("user", f"faça uma história curta de amor {User.display_name} e {User2.display_name}. Caso a história tenha um final, não continue ela.")
-                await ctx.send(history[-1]["content"])
+                async with ctx.typing():
+                    data = {"role": "user", "content":f"faça uma história curta de amor entre {User.display_name} e {User2.display_name}. Não escreva histórias incompletas, use no máximo seu limite de tokens que é de 100."}
+                    processing = Processing(data)
+                    processing.start()
+                    loop = asyncio.get_event_loop()
+                    history = await loop.run_in_executor(None, processing.future.result)
+                    content = "".join(chunk.choices[0].delta.content for chunk in history if chunk.choices[0].delta.content is not None)
+                    if(len(content) > 0):
+                        await ctx.send(content)
+                    else:
+                        await ctx.send("Erro! AI não gerou conteúdo.")
             else:
                 await ctx.send("Você precisa se registrar para usar este comando.")
-        except:
-            await ctx.send("Erro ao executar o comando. Provavelmente tem haver com o localhost não estar ligado.")
+        except Exception as e:
+            ctx.send("Ocorreu um problema inesperado!")
+
+    @commands.command()
+    async def speak(self, ctx, *, content):
+        try:
+            async with ctx.typing():
+                data ={"role" : "user", "content" : content + ". De preferência escreva mensagens curtas."}
+                processing = Processing(data)
+                processing.start()
+                loop = asyncio.get_event_loop()
+                history = await loop.run_in_executor(None, processing.future.result)
+                content = "".join(chunk.choices[0].delta.content for chunk in history if chunk.choices[0].delta.content is not None)
+                if(len(content) > 0):
+                    await ctx.send(content)
+                else:
+                    await ctx.send("Erro! AI não gerou conteúdo.")
+        except Exception as e:
+            ctx.send("Ocorreu um problema inesperado!")
+
+        
 
 
 async def setup(bot):
