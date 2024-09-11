b56e88d191c6dbed2bb6323d863163d9cc7c3a3a
Pedro Souza <pedroh.fsouzars@gmail.com>
2024-09-10 23:57:15 -0300

    refactor: removal of methods and unused imports

--- tools/pointscore.py
+++ tools/pointscore.py
@@ -7,18 +7,12 @@ from db.bankDB import Bank
 from discord.ext import commands
 from tools.shared import send_bot_embed, make_embed_object, user_cache_retriever, guild_cache_retriever, cooldown_user_tracker, format_number
 from tools.chickens.chickenshared import update_user_farm, update_player_corn
-from tools.settings import user_salary_drop
 from tools.prices import Prices
 from discord.ext.commands import Context
 from tools.listeners import on_user_transaction
-from contextlib import contextmanager
 import discord
 import logging
-import math
-import time
 logger = logging.getLogger('botcore')
-join_time = {}
-init_time = math.ceil(time.time())
 
 # This class is responsible for handling the prices of the commands.
 
@@ -126,89 +120,15 @@ async def handle_exception(ctx: Context, description: str) -> None:
     if await cooldown_user_tracker(ctx.author.id):
         await send_bot_embed(ctx, description=description)
         await refund(ctx.author, ctx)
-
-async def update_user_points(user: discord.Member, data: dict) -> dict:
-    """
-    Updates the user's points when the command is called.
-    """
-    user_data = data['user_data']
-    if user_data:
-        points = await update_points(user)
-        salary_gain = await get_salary_points(user, user_data)
-        if points:
-            user_data['points'] = points
-        else:
-            return user_data, salary_gain
-        User.update_points(user.id, user_data['points'])
-        return user_data, salary_gain
     
-async def get_salary_points(user: discord.Member, user_data: dict) -> int:
-    """
-    Calculates the salary of the user based on their roles.
-    """
-    last_title_drop = time.time() - user_data["salary_time"]
-    hours_passed = min(last_title_drop // user_salary_drop, 12)
-    hours_passed = int(hours_passed)
-    salary = await salary_role(user_data)
-    if hours_passed > 0 and user_data["roles"] != "":
-        points_gain = salary * hours_passed
-        user_data['points'] += points_gain
-        User.update_salary_time(user.id)
-        logger.info(f"{user.display_name} has received {salary * hours_passed} eggbux from their title.")
-        return points_gain
-    return 0
-    
-async def update_points(user: discord.Member) -> int:
-        """Updates the points of the user every 10 seconds."""
-        userId = user.id
-        if not user.voice and userId in join_time.keys():
-            total_points = await add_points(join_time[userId], userId)
-            join_time.pop(userId)
-            return total_points
-        
-        if userId in join_time.keys():
-            total_points = await add_points(join_time[userId], userId)
-            join_time[userId] = math.ceil(time.time())
-            return total_points
-        
-        if userId not in join_time.keys() and user.voice:
-           total_points = await add_points(init_time, userId)
-           join_time[userId] = math.ceil(time.time())
-           return total_points
-
-async def add_points(type: int, user_id: int) -> int:
-    add_points = (math.ceil(time.time()) - type) // 10
-    user_data = await user_cache_retriever(user_id)
-    user_data = user_data["user_data"]
-    total_points = user_data["points"] + add_points
-    return total_points
-
-async def salary_role(user_data: dict) -> int:
-    """Returns the salary of a user based on their roles."""
-    salarios = {
-        "T": 20,
-        "L": 40,
-        "M": 60,
-        "H": 80
-    }
-    if user_data['roles'] != "":
-        return salarios[user_data["roles"][-1]]
-    else:
-        return 0
-
-async def count_points(user: discord.Member) -> None:
-    """Counts the points of the user every time he enters a voice channel."""
-    userId = user.id
-    if user.bot:
-        return
-    if userId not in join_time.keys():
-        join_time[userId] = math.ceil(time.time())
-    else:
-        return
-
 async def automatic_register(user: discord.Member) -> None:
     """Automatically registers the user in the database."""
-    if User.read(user.id) and user.bot:
+    user_data = await user_cache_retriever(user.id)['user_data']
+    if user_data:
+        return
+    elif User.read(user.id):
+        return
+    elif user.bot:
         return
     else:
         User.create(user.id, 0)
@@ -278,7 +198,8 @@ def pricing() -> dict:
                 user_data = data['user_data']
 
                 if not user_data:
-                    await send_bot_embed(ctx, description=f":no_entry_sign: {ctx.author.display_name} is not registered in the database. Type **!register** to register or join any voice channel to register automatically.")
+                    await send_bot_embed(ctx, description=f":no_entry_sign: {ctx.author.display_name} is not registered in the bot. Try the command again.")
+                    await automatic_register(ctx.author)
                     return False
 
                 if not await get_points_commands_submodules(ctx, config_data):
@@ -295,8 +216,9 @@ def pricing() -> dict:
                 if await verify_points(command_name, user_data):
                     result = await treat_exceptions(ctx, command_name, user_data, config_data, data)
                     ctx.data = data
-                    if result: 
-                        ctx.data['user_data'], salary_gained = await update_user_points(ctx.author, data)
+                    if result:
+                        points_manager = ctx.bot.get_cog("PointsManager")
+                        ctx.data['user_data'], salary_gained = await points_manager.update_user_points(ctx.author, data)
                         if ctx.data['farm_data']:
                             ctx.data['farm_data'], total_profit = await update_user_farm(ctx, ctx.author, data)
                             corn_to_cache, corn_produced = await update_player_corn(ctx.author, data['farm_data'])
@@ -314,23 +236,3 @@ def pricing() -> dict:
             await send_bot_embed(ctx, description=":no_entry_sign: Unknown points command.")
             return False
     return commands.check(predicate)
-
-@commands.Cog.listener()
-async def on_voice_state_update(user: discord.Member, before, after):
-    """Listens to the voice state update event."""
-    guild_data = await guild_cache_retriever(user.guild.id)
-    if not guild_data['toggled_modules'] == "N":
-        user_data = await user_cache_retriever(user.id)
-        user_data = user_data["user_data"]
-        if user.bot:
-            return
-        if user_data and before.channel is None and after.channel is not None:
-            await count_points(user)
-        elif not user_data and before.channel is None and after.channel is not None:
-            await automatic_register(user)
-            await count_points(user)
-        elif user_data and before.channel is not None and after.channel is None:
-            await update_points(user)
-        elif not user_data and before.channel is not None and after.channel is None:
-            await automatic_register(user)
-            await update_points(user)
