32a0628ab26116978af8ab5ec16c2d1623a5d0fa
Pedro Souza <pedroh.fsouzars@gmail.com>
2024-09-15 20:14:17 -0300

    feat: tasks feature

--- cogs/botCore.py
+++ cogs/botCore.py
@@ -2,15 +2,13 @@
 This module contains the core of the bot, performing essential functions.
 """
 
-from discord.ext import commands
-from dotenv import load_dotenv
-from tools.shared import send_bot_embed, make_embed_object, is_dev, make_embed_object, dev_list, guild_cache_retriever, cooldown_user_tracker
+from discord.ext import commands, tasks
+from tools.shared import *
 from db.botconfigdb import BotConfig
 from tools.cache import cache_initiator
 from tools.pointscore import refund
 from tools.prices import Prices
-from tools.helpSelect import ShowPointsModules
-from colorlog import ColoredFormatter
+from tools.listeners import listener_manager
 from discord.ext.commands import Context
 import discord.ext.commands.errors
 import logging
@@ -24,36 +22,8 @@ logger = logging.getLogger('botcore')
 class BotCore(commands.Cog):
 
     def __init__(self, bot):
-        load_dotenv()
         self.bot = bot
-        self.logger = self.setup_logging()
-        self.last_cooldown_message_time = {}
-
-    def setup_logging(self) -> logging.Logger:
-        """
-        Setup the logging for the bot.
-
-        Returns:
-            logging.Logger
-        """
-        logger = logging.getLogger('botcore')
-        logger.setLevel(logging.INFO)
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.INFO)
-        formatter = ColoredFormatter(
-            "%(log_color)s%(asctime)s - %(levelname)-8s%(reset)s - %(message)s",
-            datefmt=None,
-            reset=True,
-            log_colors={
-                "DEBUG": "cyan",
-                "INFO": "green",
-                "WARNING": "yellow",
-                "ERROR": "red",
-                "CRITICAL": "purple",
-            },
-        )
-        console_handler.setFormatter(formatter)
-        logger.addHandler(console_handler)
+        self.cache_task.start()
 
     async def restart_client(self) -> None:
         """
@@ -141,84 +111,6 @@ class BotCore(commands.Cog):
         else:
             pass
         
-    @commands.command("setModule", aliases=["setM"])
-    async def set_module(self, ctx: Context) -> None:
-        """
-        Set the module where the bot will pick commands from.
-
-        Args:
-            ctx (Context): The context of the command.
-        
-        Returns:
-            None
-        """
-        guild_data = await guild_cache_retriever(ctx.guild.id)
-        if ctx.author.id == ctx.guild.owner_id:
-            embed = await make_embed_object(title="**:gear: MODULES:**", description="1. :infinity: **Total**\n2. 🐣 **Chickens**\n3. 🪩 **Interactives**\n4. :x: **None**\n\n**Important note**: \nSelect one of the modules to enable.")
-            view = ShowPointsModules(ctx.author.id, guild_data)
-            await ctx.send(embed=embed, view=view)
-        else:
-            embed = await make_embed_object(description=":no_entry_sign: You don't have the necessary permissions to use this command.")
-            await ctx.author.send(embed=embed)
-
-    @commands.command("setPrefix", aliases=["setP"])
-    async def set_prefix(self, ctx: Context, prefix: str) -> None:
-        """
-        Set the prefix for the bot.
-
-        Args:
-            ctx (Context): The context of the command.
-            prefix (str): The prefix to set.
-
-        Returns:
-            None
-        """
-        if ctx.author.guild_permissions.administrator:
-            if len(prefix) > 1:
-                await send_bot_embed(ctx, description=":no_entry_sign: The prefix can't have more than one character.")
-                return
-            BotConfig.update_prefix(ctx.guild.id, prefix)
-            await send_bot_embed(ctx, description=f":white_check_mark: Prefix has been set to **{prefix}**.")
-        else:
-            embed = await make_embed_object(description=":no_entry_sign: You don't have the necessary permissions to use this command.")
-            await ctx.author.send(embed=embed)
-    
-    @staticmethod
-    async def get_prefix_for_guild(_, message: discord.Message = None) -> str:
-     """
-     Get the prefix for the guild.
-
-    Args:
-        message (discord.Message): The message object.
-
-    Returns:
-        str
-     """
-     if message:
-        if not message.guild:
-            return "!"
-        guild_id = message.guild.id
-        guild_data = await guild_cache_retriever(guild_id)
-        return guild_data['prefix']
-            
-    @commands.command("setChannel", alias=["setC"])
-    async def set_channel(self, ctx: Context) -> None:
-        """
-        Set the channel where the bot will listen for commands.
-
-        Args:
-            ctx (Context): The context of the command.
-
-        Returns:
-            None
-        """
-        if ctx.author.guild_permissions.administrator:
-            BotConfig.update_channel_id(ctx.guild.id, ctx.channel.id)
-            await send_bot_embed(ctx, description=":white_check_mark: Commands channel has been set.")
-        else:
-            embed = await make_embed_object(description=":no_entry_sign: You don't have the necessary permissions to use this command.")
-            await ctx.author.send(embed=embed)
-
     async def log_and_raise_error(self, ctx: Context, error: Exception) -> None:
         """
         Logs the error, raises it and notifies the developers.
@@ -300,13 +192,28 @@ class BotCore(commands.Cog):
                     await self.refund_price_command_on_error(ctx)
                     await self.log_and_raise_error(ctx, error)
                     return
-                
-    @commands.Cog.listener()
-    async def on_ready(self) -> None:
+                          
+    @tasks.loop(minutes=30)
+    async def cache_task(self) -> None:
+        """
+        Periodically clears the cache.
+
+        Returns:
+            None
+        """
+        await cache_initiator.start_cache_clearing_for_users()
+        await cache_initiator.start_cache_clearing_for_guilds()
+        await listener_manager.clear_listeners()
+
+    @cache_task.before_loop
+    async def before_cache_task(self) -> None:
+        """
+        Before the cache task starts.
+
+        Returns:
+            None
+        """
         await self.bot.wait_until_ready()
-        while not self.bot.is_closed():
-            await cache_initiator.start_cache_clearing_for_users()
-            await cache_initiator.start_cache_clearing_for_guilds()
     
 async def setup(bot):
     await bot.add_cog(BotCore(bot))
