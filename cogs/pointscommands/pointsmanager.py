c2d58032649a550829b4eb67156c3f38ed93de36
Pedro Souza <pedroh.fsouzars@gmail.com>
2024-09-21 20:19:41 -0300

    feat: listener_check decorator

--- cogs/pointscommands/pointsmanager.py
+++ cogs/pointscommands/pointsmanager.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
-from tools.shared import guild_cache_retriever, user_cache_retriever
+from tools.shared import user_cache_retriever
 from db.userdb import User
+from tools.decorators import listener_checks
 import discord
 import time
 import math
@@ -15,18 +16,6 @@ class PointsManager(commands.Cog):
         self.join_time = {}
         self.init_time = math.ceil(time.time())
 
-    async def check_if_user_in_message_cache(self, user_id: int) -> bool:
-        """
-        Check if the user is in the message cache.
-
-        Args:
-            user_id (int): The id of the user to check.
-
-        Returns:
-            bool
-        """
-        return user_id in self.message_cache
-
     async def reward_points_if_possible(self, user_id: int, user_data: dict) -> int | None:
         """
         Rewards the user with points in the bot cache's system if he is in the message cache.
@@ -36,18 +25,16 @@ class PointsManager(commands.Cog):
 
         Returns:
             int | None
-        """
-        if not await self.check_if_user_in_message_cache(user_id):
-            return None
-        
+        """        
         current_time = math.ceil(time.time())
         message_time = self.message_cache[user_id]
         time_difference = current_time - message_time
         time_passed = min(time_difference // 20, 1)
+        
         if time_passed > 0:
             time_passed = int(time_passed)
             self.message_cache.pop(user_id)
-            User.update_points(user_id, user_data["points"] + time_passed)
+            await User.update_points(user_id, user_data["points"] + time_passed)
     
     async def count_points(self, user: discord.Member) -> None:
         """
@@ -97,7 +84,6 @@ class PointsManager(commands.Cog):
         total_points = int(total_points)
         return total_points
     
-
     async def update_user_points_in_voice(self, user: discord.Member, user_data: dict) -> None:
         """
         Updates the points of the user currently in a voice channel while doing a bot command.
@@ -110,8 +96,7 @@ class PointsManager(commands.Cog):
         """
         total_points = await self.update_points(user)
         if total_points > 0:
-            User.update_points(user.id, user_data["points"] + total_points)
-            user_data["points"] += total_points
+            await User.update_points(user.id, user_data["points"] + total_points)
     
     async def on_user_leaving_voice(self, user: discord.Member) -> None:
         """
@@ -128,28 +113,32 @@ class PointsManager(commands.Cog):
         total_points = await self.add_points(user)
         cache = await user_cache_retriever(user.id)
         user_data = cache["user_data"]
-        User.update_points(user.id, user_data["points"] + total_points)
+        await User.update_points(user.id, user_data["points"] + total_points)
         self.join_time.pop(user.id)
         
+    @listener_checks()
     @commands.Cog.listener()
     async def on_voice_state_update(self, user: discord.Member, before, after):
         """
         Listens to the voice state update event.
         """
-        guild_data = await guild_cache_retriever(user.guild.id)
+        cache = await user_cache_retriever(user.id)
 
-        if not guild_data['toggled_modules'] == "N":
+        if not user_data:
+            return
 
-            user_data = await user_cache_retriever(user.id)
-            user_data = user_data["user_data"]
+        if user.bot:
+            return
+        
+        user_data = cache["user_data"]
+        
+        if before.channel is None and after.channel is not None:
+            await self.count_points(user)
 
-            if user.bot:
-                return
-            if user_data and before.channel is None and after.channel is not None:
-                await self.count_points(user)
-            elif user_data and before.channel is not None and after.channel is None:
-                await self.on_user_leaving_voice(user)
-            
+        elif before.channel is not None and after.channel is None:
+            await self.on_user_leaving_voice(user)
+
+    @listener_checks()        
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         """
@@ -158,17 +147,13 @@ class PointsManager(commands.Cog):
         if message.author.bot:
             return
         
-        guild_data = await guild_cache_retriever(message.guild.id)
-
-        if guild_data['toggled_modules'] == "N":
-            return
-        
-        user_data = await user_cache_retriever(message.author.id)
-        user_data = user_data["user_data"]
+        cache = await user_cache_retriever(message.author.id)
 
         if not user_data:
             return
         
+        user_data = cache["user_data"]
+
         if message.author.id not in self.message_cache:
             self.message_cache[message.author.id] = time.time()
 
