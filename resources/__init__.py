c443b4ec05724bb4fdecb0f791afdd1e6cd81f6c
Pedro Souza <pedroh.fsouzars@gmail.com>
2024-09-29 21:08:39 -0300

    refactor: add comments

--- resources/__init__.py
+++ resources/__init__.py
@@ -1,3 +1,11 @@
-from .prices import Prices
+"""
+Package for containing all the resources for the bot.
+
+This package contains all the resources for the bot. This includes the following modules:
+* Prices: This file contains all the commands registered in the bot and their current prices.
+* Settings: This file contains all the settings variables for the bot.
+* Tips: This file contains all the tips for the bot.
+"""
+from .prices import*
 from .settings import *
 from .tips import *
