99a8a1334946580a6d1a6591ed387d9e1f9070d0
Pedro Souza <pedroh.fsouzars@gmail.com>
2024-10-01 15:11:41 -0300

    add: message_cache dict to temp

--- temp/start.py
+++ temp/start.py
@@ -1,8 +1,9 @@
 from temp import BotCache
 
-__all__ = ['cache_initiator', 'cooldown_tracker', 'steal_status']
+__all__ = ['cache_initiator', 'cooldown_tracker', 'steal_status', 'message_cache']
 
 cache_initiator = BotCache(8388608, 16777216)
 
 cooldown_tracker = {}
 steal_status = {}
+message_cache = {}
