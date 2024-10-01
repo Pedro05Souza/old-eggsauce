0c5cde41a793ba1e6893eb217ce8c91dec64c67e
Pedro Souza <pedroh.fsouzars@gmail.com>
2024-09-30 21:46:13 -0300

    refactor: add init docs

--- temp/__init__.py
+++ temp/__init__.py
@@ -1,2 +1,9 @@
+"""
+Package for the bot's temporary data.
+
+Modules included:
+* cache: Contains the cache for the bot.
+* start: Initializes the cache and other temporary data.
+"""
 from .cache import *
 from .start import *
